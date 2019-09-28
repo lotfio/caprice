071c20e0d9de02b4381711122b7964238a0df60f
lotfiolakehal <lotfiolakehal@gmail.com>
2019-09-28 19:03:19 +0100

    wip

--- src/Template.php
+++ src/Template.php
@@ -41,23 +41,37 @@ class Template
      * @param  string $file
      * @return void
      */
+<<<<<<< Updated upstream
     private function parse(string $pattern, callable $callback, string $file, int $number = -1)
     {
         return preg_replace_callback($pattern, $callback, $file, $number);
+=======
+    private function parse(string $pattern, callable $callback, string $file, int $limit = -1)
+    {
+        return preg_replace_callback($pattern, $callback, $file, $limit);
+>>>>>>> Stashed changes
     }
 
     /**
-     * parse echo
+     * parse definition block
      *
      * @return void
      */
-    public function parseEchoEscaped()
+    public function parseBlock()
     {
+<<<<<<< Updated upstream
         $pattern = "/\(\-{2}([\$\w\s\+\=\%\~\-\/\*]+)\-{2}\)/s";
 
         $this->file = $this->parse($pattern, function($match) use($pattern){
 
             return preg_replace($pattern, '<?=htmlentities("$1", ENT_QUOTES, \'UTF-8\');?>', $match[0]);
+=======
+        $pattern = "/\({2}(.*?)\){2}/s";
+
+        $this->file = $this->parse($pattern,function($match){
+
+            return '<?php '. trim($match[1]) .'?>';
+>>>>>>> Stashed changes
 
         }, $this->file);
     }
@@ -69,11 +83,35 @@ class Template
      */
     public function parseEcho()
     {
+<<<<<<< Updated upstream
         $pattern = "/\(\-{1}([\$\w\s\+\=\%\~\-\/\*\|]+)\-{1}\)/s";
 
         $this->file = $this->parse($pattern, function($match) use ($pattern){
 
             return preg_replace($pattern, '<?="$1"?>', $match[0]);
+=======
+        $pattern = "/\(\-{1}(.*?)\-{1}\)/s";
+
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?="'.trim($match[1]).'"?>';
+
+        }, $this->file);
+    }
+
+    /**
+     * parse echo
+     *
+     * @return void
+     */
+    public function parseEchoEscaped()
+    {
+        $pattern = "/\(\={1}(.*?)\={1}\)/s";
+
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?=htmlentities("'.trim($match[1]).'", ENT_QUOTES, \'UTF-8\');?>';
+>>>>>>> Stashed changes
 
         }, $this->file);
     }
@@ -86,11 +124,19 @@ class Template
     public function parseFor()
     {
         $pattern  = '/#for\s*\((\$\w+\s*=\s*[\$\w+]+\s*\;)(\s*\$\w+\s*[<=>!]+\s*[\$\w+]+\s*\;\s*)(\$\w+[+-=\/\*\s\w\$]+)\)(.*?)#endfor/s';
+<<<<<<< Updated upstream
 
         $this->file = $this->parse($pattern, function($match) use($pattern){
 
             $match = preg_replace($pattern, '<?php for($1$2$3):?>$4<?php endfor;?>', $match[0]);
             return $match;
+=======
+        
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?php for('.trim($match[1]).''. trim($match[2]).''.trim($match[3]).'):?>'.trim($match[4]).'<?php endfor;?>';
+
+>>>>>>> Stashed changes
         }, $this->file);
     }
 
@@ -102,10 +148,18 @@ class Template
     public function parseForInKeyValue()
     {
         $pattern  = '/#for\s*\((\$\w+\s*=>\s*\$\w+\s*)(\s*in\s*)(\$\w+\s*)\)(.*?)#endfor/s';
+<<<<<<< Updated upstream
 
         $this->file = $this->parse($pattern, function($match) use ($pattern){
             $match = preg_replace($pattern, '<?php foreach($3 as $1):?>$4<?php endforeach;?>', $match[0]);
             return $match;
+=======
+        
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?php foreach('.trim($match[3]).' as '.trim($match[1]).'):?>'.trim($match[4]).'<?php endforeach;?>';
+
+>>>>>>> Stashed changes
         }, $this->file);
     }
 
@@ -117,10 +171,18 @@ class Template
     public function parseForInValueOnly()
     {
         $pattern  = '/#for\s*\((\$\w+\s*)(\s*in\s*)(\$\w+\s*)\)(.*?)#endfor/s';
+<<<<<<< Updated upstream
 
         $this->file = $this->parse($pattern, function($match) use ($pattern){
             $match = preg_replace($pattern, '<?php foreach($3 as $1):?>$4<?php endforeach;?>', $match[0]);
             return $match;
+=======
+        
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?php foreach('.trim($match[3]).' as '.trim($match[1]).'):?>'.trim($match[4]).'<?php endforeach;?>';
+        
+>>>>>>> Stashed changes
         }, $this->file);
     }
 
@@ -132,10 +194,18 @@ class Template
     public function parseWhile()
     {
         $pattern  = '/#while\s*\(([\$\w+\d+\s*\<\=\>\!]+)\)(.+?)#endwhile/s';
+<<<<<<< Updated upstream
 
         $this->file = $this->parse($pattern, function($match) use($pattern){
 
             return preg_replace($pattern, '<?php while($1):?>$2<?php endwhile;?>', $match[0]);
+=======
+        
+        $this->file = $this->parse($pattern, function($match){
+
+            return '<?php while('.trim($match[1]).'):?>'.trim($match[2]).'<?php endwhile;?>';
+
+>>>>>>> Stashed changes
         }, $this->file);
     }
 
@@ -146,14 +216,26 @@ class Template
      */
     public function parseIf()
     {
+<<<<<<< Updated upstream
         $pattern  = '/#if\s*\((.*?)\)(.*?)#endif/s';
 
         $this->file = $this->parse($pattern, function($match) use($pattern){
             return preg_replace($pattern, '<?php if($1):?> $2 <?php endif;?>', $match[0]);
         }, $this->file);
     }
+=======
+        $pattern    = '/(#if)\s*\((.*?)\)/';
+        $this->file = $this->parse($pattern, function($match){
+            return '<?php if('.trim($match[2]).'):?>';
+        }, $this->file);
+>>>>>>> Stashed changes
 
+        $pattern    = '/(#elif)\s*\((.*?)\)/';
+        $this->file = $this->parse($pattern, function($match){
+            return '<?php elseif('.trim($match[2]).'):?>';
+        }, $this->file);
 
+<<<<<<< Updated upstream
     /**
      * parse definition block
      *
@@ -169,6 +251,13 @@ class Template
             return str_replace("))", "?>", $str);
 
         }, $this->file);
+=======
+        $pattern    = '/#else*/';
+        $this->file = $this->parse($pattern, function($match) { return '<?php else:?>'; }, $this->file);
+
+        $pattern    = '/#endif/';
+        $this->file = $this->parse($pattern, function($match){return '<?php endif;?>';}, $this->file);
+>>>>>>> Stashed changes
     }
 
     public function generateParsedFile(string $name)
