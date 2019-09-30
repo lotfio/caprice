e48068248ecd547d8d0525552c7db9c73d3b914c
lotfio <a.lakehal@createam-dz.com>
2019-09-30 08:17:02 +0100

    wip

---
+++ src/Parser.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.1.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+namespace Caprice;
+
+class Parser implements ParserInterface
+{
+
+}
