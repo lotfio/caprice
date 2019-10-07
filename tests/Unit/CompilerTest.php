cd028ab8a45f21b1a9ecf1f3bb8214bcc586a1f8
lotfio <contact@lotfio.net>
2019-10-07 21:53:18 +0100

    adding basic test files

---
+++ tests/Unit/CompilerTest.php
@@ -0,0 +1,20 @@
+<?php namespace Tests\Unit;
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.1.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class CompilerTest implements TestCase
+{
+
+}
