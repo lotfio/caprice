6cadd98161a75503ea8d70ee383e3ad29963677f
lotfio <contact@lotfio.net>
2019-10-12 11:25:23 +0100

    adding extends directive

---
+++ src/Directives/ExtendsStatement.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Caprice\Directives;
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.1.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+use Caprice\Contracts\DirectiveInterface;
+
+class ExtendsStatement implements DirectiveInterface
+{
+    /**
+     * pattern property.
+     *
+     * @var string
+     */
+    public $pattern = '/#extends\s*\((.+?)\)/';
+
+    /**
+     * string to replace the pattern.
+     *
+     * @return string
+     */
+    public function replace(array $match, string $file) : string
+    {
+        $content = file_get_contents($match[1]);
+        
+        return $content;
+    }
+}
