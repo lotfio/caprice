c65e2f6d980680d1286c3c4cabb5491188d65aa0
lotfio <contact@lotfio.net>
2019-10-12 11:26:00 +0100

    adding utils class

---
+++ src/Utils.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Caprice;
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.1.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+class Utils
+{
+    /**
+     * hide sections
+     *
+     * @param  string $file
+     * @return string
+     */
+    public static function hideSections(string $file) : string
+    {
+        return preg_replace('/#section\s*\((".*?")\)(.*?)#endsection/s', NULL, $file);
+    }
+}
