8b56cf18255ad05b3fc6778c6e704eb6840fe3bc
lotfio <a.lakehal@createam-dz.com>
2020-02-06 15:28:50 +0100

    adding remove Htmlcomment directive

---
+++ src/Directives/HtmlCommentStatement.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Caprice\Directives;
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.2.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+use Caprice\Contracts\DirectiveInterface;
+
+class HtmlCommentStatement implements DirectiveInterface
+{
+    /**
+     * pattern property.
+     *
+     * @var string
+     */
+    public $pattern = '/<!--(.*?)-->/s';
+
+    /**
+     * directive replace method.
+     *
+     * @param array  $match
+     * @param string $file     original file
+     * @param string $filesDir .cap files dir
+     *
+     * @return string
+     */
+    public function replace(array $match, string $file, string $filesDir) : string
+    {
+        return '';
+    }
+}
