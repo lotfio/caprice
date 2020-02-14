6ec95399cd5da941f717deaadfca27c98a164100
lotfio <contact@lotfio.net>
2020-02-14 12:11:03 -0800

    adding do while

---
+++ src/Directives/DoWhileStatement.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Caprice\Directives;
+
+/*
+ * This file is a part of Caprice package
+ *
+ * @package     Caprice
+ * @version     0.3.0
+ * @author      Lotfio Lakehal <contact@lotfio.net>
+ * @copyright   Lotfio Lakehal 2019
+ * @license     MIT
+ * @link        https://github.com/lotfio/caprice
+ *
+ */
+
+use Caprice\Contracts\DirectiveInterface;
+
+class DoWhileStatement implements DirectiveInterface
+{
+    /**
+     * pattern property.
+     *
+     * @var string
+     */
+    public $pattern = '/#do(.*?)#while\s*\(([\$\w+\d+\s*\<\=\>\!\)\(\,\.\-\|]+)\)/s';
+
+    /**
+     * directive replace method.
+     *
+     * @param array  $match
+     * @param string $file     original file
+     * @param string $filesDir .cap files dir
+     *
+     * @return string
+     */
+    public function replace(array $match, string $file, string $filesDir): string
+    {
+        return '<?php do{ ?> '.trim($match[1]).' <?php } while ( '.trim($match[2]).' );?>';
+    }
+}
