2548096cda018fbe8ae3696764ec7a1bc712c4af
lotfio <contact@lotfio.net>
2020-09-20 11:42:08 +0100

    wip examples

--- index.cap.php
+++ index.cap.php
@@ -6,7 +6,44 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+    <!-- code block -->
+    #php 
+        $names = ['foo', 'bar', 'baz'];
+        $age   = 15; 
+    #endphp
+
+    <hr>
+    <!-- for loop -->
+    #for($i = 0; $i < count($names); $i++)
+        - {{ $i }}  {{ $names[$i] }} <br>
+    #endfor
+
+    <hr>
+    <!-- for  in loop -->
+    #for($indx => $name in $names)
+        - {{ $indx }} {{ $name }} <br>
+    #endforin
+
+    <hr>
+    <!-- while loop -->
+    #php $i = 0 #endphp
+    #while($i < count($names))
+
+        - {{ $i }} {{ $names[$i] }} <br>
+
+        #php $i++ #endphp 
+    #endwhile
+
+    <hr>
+    <!-- do while loop -->
+    #php $i = 0 #endphp
+    #do
+
+        - {{ $i }} {{ $names[$i] }} <br>
+
+        #php $i++ #endphp 
+    #enddo($i < count($names))
 
 </body>
 </html>
